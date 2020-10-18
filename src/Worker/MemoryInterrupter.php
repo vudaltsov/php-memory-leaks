c9954bba4315a266e8deec9fbd9ebdec9363ad1d
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2020-10-18 16:49:37 +0300

    Moved MemoryInterrupter to Worker namespace

--- src/Worker/MemoryInterrupter.php
+++ src/Worker/MemoryInterrupter.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App;
+namespace App\Worker;
 
 use Psr\Log\LoggerInterface;
 use Psr\Log\NullLogger;
---
+++ src/Worker/Worker.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Worker;
+
+final class Worker
+{
+    public function stop(): void
+    {
+    }
+}
